bafffaa2e4220bd2d9afdb2129cf21dacccc799f
jonathanhenri <jonathan.melosouza@gmail.com>
2017-04-18 00:17:52 -0300

    tcc

---
+++ src/main/java/com/mycompany/visao/cadastro/aluno/CadastroAlunoPanel.java
@@ -0,0 +1,10 @@
+package com.mycompany.visao.cadastro.aluno;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class CadastroAlunoPanel extends Panel {
+
+	public CadastroAlunoPanel(String id) {
+		super(id);
+	}
+}
